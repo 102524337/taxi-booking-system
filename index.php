a81f981c5aee6294d0ab261ca6382e4b22829284
102524337 <akdrn3212@gmail.com>
2020-06-18 17:49:28 +1000

    commit

--- index.php
+++ index.php
@@ -3,10 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css"/>
     <title>Welcome to Taxi Service</title>
 </head>
 <body>
-    <h1>Book a taxi</h1>
-    <a href="register.php">register now</a>
+    <nav>
+        <a href="register.php">register</a>
+        <a href="login.php">Log In</a>
+    </nav>
 </body>
 </html>
