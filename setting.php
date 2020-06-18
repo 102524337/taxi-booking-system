1827b087b3bda81536d34dc5f2f302ac850fefdf
102524337 <akdrn3212@gmail.com>
2020-06-18 17:17:33 +1000

    commited

--- setting.php
+++ setting.php
@@ -1,8 +1,8 @@
 <?php
-	$host = "feenix-mariadb.swin.edu.au";
-	$user = "s102524337"; 
-	$pwd = "hahaha3212"; 
-	$sql_db = "s102524337_db"; 
+	$host = "gtizpe105piw2gfq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+	$user = "zywzxtuw4bf8b91s"; 
+	$pwd = "utl264hy5sb5kw7x"; 
+	$sql_db = "nibmkh6atkvyl8tj"; 
 
 	$conn = @mysqli_connect(
 		$host,
